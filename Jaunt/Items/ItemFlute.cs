b6134f76dbd36a91a2348fc6f3e1007474b16c86
safwyls <safwyl@pm.me>
2025-06-30 18:00:15 -0700

    Add special flute class and assets

---
+++ Jaunt/Items/ItemFlute.cs
@@ -0,0 +1,151 @@
+﻿using System.Linq;
+using Jaunt.Behaviors;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
+using Vintagestory.API.Config;
+using Vintagestory.API.MathTools;
+using Vintagestory.GameContent;
+
+#nullable disable
+
+namespace Jaunt.Items
+{
+    public class ItemFlute : Item
+    {
+        protected static JauntModSystem ModSystem => JauntModSystem.Instance;
+        SkillItem[] modes;
+        
+        public override void OnLoaded(ICoreAPI api)
+        {
+            ModSystem.Logger.Notification("Initializing Jaunt ItemFlute...");
+            base.OnLoaded(api);
+            var capi = api as ICoreClientAPI;
+
+            modes = new[]
+            {
+                new SkillItem()
+                {
+                    Code = new AssetLocation("play"),
+                    Name = Lang.Get("instrument-skill-play")
+                },
+                new SkillItem()
+                {
+                    Code = new AssetLocation("bind"),
+                    Name = Lang.Get("instrument-skill-bind")
+                }
+            };
+
+            if (capi != null)
+            {
+                modes[0].WithIcon(capi, capi.Gui.LoadSvgWithPadding(new AssetLocation("textures/icons/instrument-play.svg"), 48, 48, 5, ColorUtil.WhiteArgb));
+                modes[0].TexturePremultipliedAlpha = false;
+                modes[1].WithIcon(capi, capi.Gui.LoadSvgWithPadding(new AssetLocation("textures/icons/instrument-bind.svg"), 48, 48, 5, ColorUtil.WhiteArgb));
+                modes[1].TexturePremultipliedAlpha = false;
+            }
+        }
+
+        public override void OnHeldInteractStart(ItemSlot slot, EntityAgent byEntity, BlockSelection blockSel, EntitySelection entitySel, bool firstEvent, ref EnumHandHandling handling)
+        {
+            bool playmode = slot.Itemstack.Attributes.GetInt("toolMode", 0) == 0;
+
+            if (playmode)
+            {
+                var ela = api.World.ElapsedMilliseconds;
+                var prevela = slot.Itemstack.Attributes.GetLong("lastPlayerMs", -99999);
+
+                // User must have restarted his game world, allow call
+                if (prevela > ela)
+                {
+                    prevela = ela-4001;
+                }
+                if (ela - prevela <= 4000) return;
+
+            
+                slot.Itemstack.Attributes.SetLong("lastPlayerMs", ela);
+                api.World.PlaySoundAt(new AssetLocation("sounds/instrument/elkcall"), byEntity, (byEntity as EntityPlayer)?.Player, 0.75f, 32, 0.5f);
+
+                if (api.Side == EnumAppSide.Server)
+                {
+                    callElk(slot, byEntity);
+                }   
+            }
+            else
+            {
+                SetBoundEntityType(slot, entitySel.Entity);
+            }
+
+            handling = EnumHandHandling.PreventDefault;
+        }
+        
+        public override SkillItem[] GetToolModes(ItemSlot slot, IClientPlayer forPlayer, BlockSelection blockSel)
+        {
+            return modes;
+        }
+        
+        public override void SetToolMode(ItemSlot slot, IPlayer byPlayer, BlockSelection blockSelection, int toolMode)
+        {
+            slot.Itemstack.Attributes.SetInt("toolMode", toolMode);
+        }
+
+        public override int GetToolMode(ItemSlot slot, IPlayer byPlayer, BlockSelection blockSelection)
+        {
+            return slot.Itemstack.Attributes.GetInt("toolMode", 0);
+        }
+
+        public void SetBoundEntityType(ItemSlot slot, Entity entity)
+        {
+            var groupCode = entity.GetBehavior<EntityBehaviorOwnable>()?.Group;
+            
+            if (string.IsNullOrEmpty(groupCode)) return;
+            
+            slot.Itemstack.Attributes.SetString("groupCode", groupCode);
+        }
+
+        private void callElk(ItemSlot slot, EntityAgent byEntity)
+        {
+            var groupCode = slot.Itemstack.Attributes.GetString("groupCode", "mountableanimal");
+            var plr = (byEntity as EntityPlayer).Player;
+            var mseo = api.ModLoader.GetModSystem<ModSystemEntityOwnership>();
+            if (!mseo.OwnerShipsByPlayerUid.TryGetValue(plr.PlayerUID, out var ownerships) || ownerships == null || !ownerships.TryGetValue(groupCode, out var ownership))
+            {
+                return;
+            }
+
+            var entity = api.World.GetEntityById(ownership.EntityId);
+            if (entity == null)
+            {
+                return;
+            }
+
+            var mw = entity.GetBehavior<EntityBehaviorMortallyWoundable>();
+            if (mw?.HealthState == EnumEntityHealthState.MortallyWounded || mw?.HealthState == EnumEntityHealthState.Dead)
+            {
+                return;
+            }
+
+            var tm = entity.GetBehavior<EntityBehaviorTaskAI>().TaskManager;
+            var aitcto = tm.AllTasks.FirstOrDefault(t => t is AiTaskComeToOwner) as AiTaskComeToOwner;
+            if (entity.ServerPos.DistanceTo(byEntity.ServerPos) > aitcto.TeleportMaxRange) // Do nothing outside max teleport range
+            {
+                return;
+            }
+
+            var mount = entity?.GetInterface<IMountable>();
+            if (mount != null)
+            {
+                if (mount.IsMountedBy(plr.Entity)) return;
+                if (mount.AnyMounted())
+                {
+                    entity.GetBehavior<EntityBehaviorRideable>()?.UnmnountPassengers(); // You are not my owner, get lost!
+                }
+            }
+
+            entity.AlwaysActive = true;
+            entity.State = EnumEntityState.Active;
+            aitcto.allowTeleportCount=1;
+            tm.StopTasks();
+            tm.ExecuteTask(aitcto, 0);
+        }
+    }
+}
