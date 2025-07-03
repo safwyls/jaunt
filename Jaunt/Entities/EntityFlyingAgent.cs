b7cff6486ba72b019c7a1de5fa3aeaa094c5bd60
safwyls <safwyl@pm.me>
2025-07-02 19:53:08 -0700

    Disable step pitch on flying mounts

--- Jaunt/Entities/EntityFlyingAgent.cs
+++ Jaunt/Entities/EntityFlyingAgent.cs
@@ -4,6 +4,7 @@ namespace Jaunt.Entities;
 
 public class EntityFlyingAgent : EntityAgent
 {
+    public override bool CanStepPitch => false;
     public override bool CanSwivel => true;
     public override bool CanSwivelNow => true;
 }
