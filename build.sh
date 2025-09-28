954f995bd07d48a98e60a1f17eb7618e63020358
sekelsta <sekelsta@protonmail.com>
2025-09-28 14:38:11 -0500

    Clarify intentions for different environment variables here

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
+# Intention: VINTAGE_STORY points to your Vintage Story install location
+# while VINTAGE_STORY_DATA points to your Vintage Story data location
+# Depending on how you set up those environment variables, you could use
+# a different game install between playing and devving, or just a different
+# data folder location (using the --dataPath argument on startup)
+
 dotnet run --project ./CakeBuild/CakeBuild.csproj -- "$@"
 rm -r Jaunt/bin/
 rm -r Jaunt/obj/
-rm "${VINTAGE_STORY}"/Mods/jaunt_*.zip
-cp Releases/jaunt_*.zip "${VINTAGE_STORY}/Mods"
+
+VS_DATA=${VINTAGE_STORY_DATA:-~/.config/VintagestoryData}
+rm "${VS_DATA}"/Mods/jaunt_*.zip
+cp Releases/jaunt_*.zip "${VS_DATA}/Mods"
