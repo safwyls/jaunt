afaba56754771bb8dd8bfed7c4b83532587c6fc8
jayhide <59358264+jayhide@users.noreply.github.com>
2025-12-22 07:23:16 -0800

    Stop resetting of saddle breaks on restart. (#16)

--- Jaunt/Behaviors/EntityBehaviorJauntRideable.cs
+++ Jaunt/Behaviors/EntityBehaviorJauntRideable.cs
@@ -118,9 +118,10 @@ namespace Jaunt.Behaviors
 
             base.Initialize(properties, attributes);
 
+
             if (attributes["saddleBreaksRequired"].Exists)
             {
-                if (!entity.WatchedAttributes.HasAttribute("requiredSaddleBreaks") && api.Side == EnumAppSide.Server)
+                if (!entity.WatchedAttributes.HasAttribute("remainingSaddleBreaksRequired") && api.Side == EnumAppSide.Server)
                 {
                     RemainingSaddleBreaks = GameMath.RoundRandom(api.World.Rand, attributes["saddleBreaksRequired"].AsObject<NatFloat>().nextFloat(1, api.World.Rand));
                 }
